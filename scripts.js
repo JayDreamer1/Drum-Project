da85035b5da68b58e12ab224d4bb8a5e667d6f6b
JayDreamer1 <gyve.sonjay@gmail.com>
2024-01-29 19:05:02 +0800

    first commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function makeSound(e) {
+    const playAudio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if(!playAudio) return
+    playAudio.currentTime = 0;
+    key.classList.add('playing')
+    playAudio.play();
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    if(this.classList.contains('playing')) this.classList.remove('playing')
+}
+const btnStatus = document.querySelectorAll('.key');
+btnStatus.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', makeSound);
